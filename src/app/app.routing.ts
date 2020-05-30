580986d88ba0ea320139019a54d2bbaa31fb9c78
Bruno Paganotti <bpaganotti@gmail.com>
2020-05-30 13:56:00 -0300

    initial

---
+++ src/app/app.routing.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes =[
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home',             component: HomeComponent }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes,{
+      useHash: false
+    })
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
